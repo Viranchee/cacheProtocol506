4e6a88e2c3107c96d84a7dfd6f5c2dd61814961e
Viranchee Lotia <viranchee@outlook.com>
2022-03-24 12:47:36 -0400

    Add the cache associativity numbers

---
+++ ran/addition.py
@@ -0,0 +1,52 @@
+# List all files in the directory
+import os
+files = os.listdir()
+# filter txt from files
+txt_files = list(filter(lambda x: x.endswith('.txt'), files))
+sorted_files = sorted(txt_files)
+
+def add_numbers_in_file(fileName, outputFile):
+    # Dictionary to store Key Value pairs
+    initial_lines = []
+    totals = {}
+    # Print all lines from aFile
+    with open(fileName) as f:
+        # Add first 7 lines to initial_lines
+        for i in range(7):
+            initial_lines.append(f.readline())
+        for line in f:
+            # If line starts with number
+            if line[0].isdigit():
+                # split line by colon
+                line = line.split(':')
+                # Get the key
+                key = line[0]
+                # Get the value
+                value = line[1]
+                # Add key and value to totals
+                if key in totals:
+                    totals[key] += float(value)
+                else:
+                    totals[key] = float(value)
+
+    # Modify initial_line line 5 and 6, replace one \t with 2 \t
+    initial_lines[4] = initial_lines[4].replace('\t', '\t\t')
+    initial_lines[5] = initial_lines[5].replace('\t', '\t\t')
+    # Append initial_line, totals to outputFile
+    with open(outputFile, 'a') as f:
+        f.writelines(initial_lines)
+        # Add newline
+        f.write('\n')
+        for key, value in totals.items():
+            f.write(key + ':\t\t' + str(int(value)) + '\n')
+        # Add 3 newlines
+        f.write('\n' * 3)
+
+    initial_lines = [] 
+    totals = {} 
+
+# add_numbers_in_file(txt_files[0], 'output.txt')
+
+# Call add_numbers_in_file for each txt file
+for file in sorted(txt_files):
+    add_numbers_in_file(file, 'output.txt')
