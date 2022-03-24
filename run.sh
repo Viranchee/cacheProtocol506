7406c9da137d298f9669f296efb903342a2478d8
Viranchee Lotia <viranchee@outlook.com>
2022-03-24 01:02:28 -0400

    Script to run on various cache sizes

--- run.sh
+++ run.sh
@@ -1,15 +1,67 @@
-# Run with 0 errors MSI
+#!/bin/bash
 
-# echo "Running MSI"
-# sh module.sh smp 128000 1 64 16 0 /afs/eos.ncsu.edu/lockers/workspace/csc/CSC506-1/trace/swaptions_truncated 1000
-# echo "COMPLETE MSI RUN"
+# Run the program
 
-# make clean
-# make
-echo "Running Dragon"
-sh module.sh smp 128 1 64 16 3 /afs/eos.ncsu.edu/lockers/workspace/csc/CSC506-1/trace/swaptions_truncated 1000
-echo "COMPLETE Dragon RUN"
+# Arguments: Cache Size, Cache Associativity, Block Size, Number of Threads, Protocol
+runAll() {
+    echo "Cache Size: $1, CacheAssoc: $2, Block Size: $3"
 
-echo "Running MOSI"
-sh module.sh smp 128 1 64 16 1 /afs/eos.ncsu.edu/lockers/workspace/csc/CSC506-1/trace/swaptions_truncated 1000
-echo "COMPLETE MOSI RUN"
+    # sh module.sh project_name cache_size assoc block_size num_processors protocol trace_file number_of_references
+
+    echo "MSI"
+    sh module.sh smp $1 $2 $3 16 0 /afs/eos.ncsu.edu/lockers/workspace/csc/CSC506-1/trace/swaptions_truncated
+    sleep 0.5
+    mv result.txt ran/msi-Cache$1-Assoc$2-Block$3.txt
+
+    echo "MOSI"
+    sh module.sh smp $1 $2 $3 16 1 /afs/eos.ncsu.edu/lockers/workspace/csc/CSC506-1/trace/swaptions_truncated
+    sleep 0.5
+    mv result.txt ran/mosi-Cache$1-Assoc$2-Block$3.txt
+
+    echo "Firefly"
+    sh module.sh smp $1 $2 $3 16 2 /afs/eos.ncsu.edu/lockers/workspace/csc/CSC506-1/trace/swaptions_truncated
+    sleep 0.5
+    mv result.txt ran/firefly-Cache$1-Assoc$2-Block$3.txt
+
+    echo "Dragon"
+    sh module.sh smp $1 $2 $3 16 3 /afs/eos.ncsu.edu/lockers/workspace/csc/CSC506-1/trace/swaptions_truncated
+    sleep 0.5
+    mv result.txt ran/dragon-Cache$1-Assoc$2-Block$3.txt
+}
+
+# Block Size Default: 64
+# Block Size: 32,64,128,256,512
+
+# Associativity Default: 1
+# Associativity: 1,2,4
+
+# Cache Size Default: 256
+# Caches: 128, 256, 512
+
+varyCacheSize() {
+    # Cache Size: 128kb 256kb 512kb
+    runAll 128 1 64
+    # runAll 256 1 64
+    # runAll 512 1 64
+}
+
+varyAssociativity() {
+    # Associativity: 1way 2way 4way
+    runAll 256 1 64
+    runAll 256 2 64
+    runAll 256 4 64
+}
+
+varyBlockSize() {
+    # Block Size: 32 64 128 256
+    runAll 256 1 32
+    runAll 256 1 64
+    runAll 256 1 128
+    runAll 256 1 256
+}
+
+make clean
+make
+varyCacheSize
+varyAssociativity
+varyBlockSize
